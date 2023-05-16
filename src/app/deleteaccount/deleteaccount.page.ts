6571cf783267394c62030db0f7ffa2a6cb053685
moavizq5555 <57653982+Najam-khan@users.noreply.github.com>
2023-05-16 10:34:14 +0500

    commit
    change in delete account page

--- src/app/deleteaccount/deleteaccount.page.ts
+++ src/app/deleteaccount/deleteaccount.page.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { ExtraService } from '../services/extra.service';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-deleteaccount',
@@ -13,14 +14,36 @@ import { ExtraService } from '../services/extra.service';
 })
 export class DeleteaccountPage implements OnInit {
   name: any;
-
+  delreason: any = '';
+  desc: any = '';
+  email: any;
   Checkboxes = [{ question: 'I concerned about my personal data', isItemChecked: false }, { question: 'I have another  Countr account', isItemChecked: false },
   { question: 'I want to remove app from my mobile', isItemChecked: false }, { question: 'I get too many emails from Countr', isItemChecked: false }, { question: 'Other', isItemChecked: false }];
 
   selectquestions: any[] = [];
-  constructor(public extra: ExtraService) { }
+  userdetail: any;
+  constructor(public extra: ExtraService,
+    public api: ApiService) { }
 
   ngOnInit() {
+    this.getProfile()
+  }
+  getProfile() {
+    this.extra.loadershow()
+    this.api.sendRequest('users_customers_profile_by_id', { "users_customers_id": localStorage.getItem('loggedId') }).subscribe((res: any) => {
+      console.log('rssss----', res);
+      this.userdetail = res.data
+      if (res.status == 'success') {
+        this.extra.hideLoader()
+        this.email = this.userdetail.email
+      } else {
+        this.extra.hideLoader()
+        this.extra.presentToast(res.message)
+      }
+
+    }, err => {
+      this.extra.hideLoader()
+    })
   }
   verifyEvent(ev: any) {
     console.log(ev);
@@ -40,14 +63,27 @@ export class DeleteaccountPage implements OnInit {
   }
 
   delete() {
-    if (this.selectquestions.length == 0) {
-      this.extra.presentToast('Please Choose atleast one')
-    } else {
-
-      this.extra.presentToast("Your account will be deleted in 24 hour's")
+    if (this.delreason == '') {
+      this.extra.presentToast('Delete reason required')
     }
+    else if (this.desc == '') {
+      this.extra.presentToast('Comments required')
+    }
+    else {
+      let data = {
+        "user_email": this.email,
+        "delete_reason": this.delreason,
+        "comments": this.desc
+      }
+      this.api.sendRequest('delete_account', data).subscribe((res: any) => {
 
-
+        if (res.status == 'success') {
+          this.extra.presentToast(res.message)
+        } else {
+          this.extra.presentToast(res.message)
+        }
+      })
+    }
   }
 
 }
